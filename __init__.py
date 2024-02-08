c216636aa7b768392b0039bd5a5b7fbd51145d3a
rocksurvie <theoalsac@icloud.com>
2024-02-08 09:50:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,41 @@ def resume_1():
 def resume_2():
     return render_template("resume_2.html")
 
+
+@app.route('/fiche_client/<int:api_key>')
+def sha256_hash(input_string):
+    # Fonction de hachage SHA-256
+    sha256 = hashlib.sha256()
+    sha256.update(input_string.encode('utf-8'))
+    return sha256.hexdigest()
+
+def check_sum_256(array, api_value, key_to_check):
+    # Calcul de la somme
+    array_sum = array[0] + api_value
+
+    # Hachage de la somme avec SHA-256
+    hashed_sum = sha256_hash(str(array_sum))
+
+    # Vérification avec la clé spécifique
+    if hashed_sum == key_to_check:
+        return True
+    else:
+        return False
+    
+    
+def Readfiche(api_key):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients')
+    data = cursor.fetchall()
+    
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
+
+
 @app.route('/resume_template')
 def resume_template():
     return render_template("resume_template.html")
