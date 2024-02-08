bb421c26207ecc9cc202ffaab0c551c771a19da3
rocksurvie <theoalsac@icloud.com>
2024-02-08 11:51:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request, redirect
 import hashlib
 import sqlite3
 
-
-app = Flask(__name__) #creating flask app name
+app = Flask(__name__)
 
 @app.route('/resume_2')
 def home():
@@ -17,28 +16,16 @@ def resume_1():
 def resume_2():
     return render_template("resume_2.html")
 
-
-@app.route('/consultation/<int:api_key>')
 def sha256_hash(input_string):
-    # Fonction de hachage SHA-256
     sha256 = hashlib.sha256()
     sha256.update(input_string.encode('utf-8'))
     return sha256.hexdigest()
 
 def check_sum_256(array, api_value, key_to_check):
-    # Calcul de la somme
     array_sum = str(array[0]) + str(api_value)
-
-    # Hachage de la somme avec SHA-256
     hashed_sum = sha256_hash(str(array_sum))
+    return hashed_sum == key_to_check
 
-    # Vérification avec la clé spécifique
-    if hashed_sum == key_to_check:
-        return True
-    else:
-        return False
-    
-"""5625719273"""
 @app.route('/consultation/<int:api_key>')
 def afficheListe(api_key):
     conn = sqlite3.connect('database.db')
@@ -46,12 +33,10 @@ def afficheListe(api_key):
     cursor.execute('SELECT * FROM clients')
     data = cursor.fetchall()
     conn.close()
-    array_sum = str(data[0]) + "5625719273"
-    if check_sum_256(data, api_key, sha256_hash(array_sum)): 
-        # Rendre le template HTML et transmettre les données
+
+    if api_key == 5625719273:
         return render_template('read_data.html', data=data)
     return "api_key !!"
-
 
 @app.route('/graphique/<int:api_key>')
 def afficheGraph(api_key):
@@ -60,12 +45,9 @@ def afficheGraph(api_key):
     cursor.execute('SELECT * FROM clients')
     data = cursor.fetchall()
     conn.close()
-    array_sum = str(data[0]) + "5625719273"
-    if check_sum_256(data, api_key, sha256_hash(array_sum)): 
-        # Rendre le template HTML et transmettre les données
+    if api_key == 5625719273:
         return render_template('graph.html', data=data)
     return "api_key !!"
-
 
 @app.route('/ajouter_message', methods=['POST'])
 def ajouter_client():
@@ -75,20 +57,15 @@ def ajouter_client():
 
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-
     cursor.execute("INSERT INTO clients (nom, prenom, messages) VALUES (?, ?, ?)", (nom, prenom, messages))
-
     conn.commit()
     conn.close()
 
-    return redirect('/ajouter_message')
-
-
-
+    return redirect('/resume_2')
 
 @app.route('/resume_template')
 def resume_template():
     return render_template("resume_template.html")
 
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     app.run()
